dafaa272df92bf396f366226f9fff2c7cb7815fa
Evgeniy <antonm33@yandex.ru>
2020-09-30 23:06:22 +0200

    fix form focus

--- src/features/messages/Form.jsx
+++ src/features/messages/Form.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useRef, useEffect } from 'react';
 import { useDispatch, connect } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
+import { Field, reduxForm, SubmissionError } from 'redux-form';
 import { fetchMessage } from './messagesSlice';
 import UserContext from '../../UserContext';
 
@@ -21,23 +21,27 @@ const validate = (values) => {
 		errors.message = 'Can`t send empty message';
 	}
 	return errors;
-}
-
+};
 const renderField = (field) => (
 	<div className='input-row'>
-		<input {...field.input} ref={inputEl} type='text' className='w-100' />
-		{field.meta.touched && field.meta.error && <span className='error text-warning'>{field.meta.error}</span>}
+		<input {...field.input} type='text' className='w-100' />
+		{field.meta.touched && field.meta.error && (
+			<span className='error text-danger'>{field.meta.error}</span>
+		)}
 	</div>
 );
 
 const Form = (props) => {
-	const { handleSubmit, submitting, SubmissionError } = props;
+	const { handleSubmit, submitting } = props;
 	const dispatch = useDispatch();
 	const userName = useContext(UserContext);
-	const inputEl = useRef(null);
 	const { reset, currentChannelId } = props;
 
 	const handleSubmitForm = async (values) => {
+		if (!values.hasOwnProperty('message')) {
+			throw new SubmissionError({ message: 'Can`t send empty message' });
+		}
+
 		const time = new Date().toLocaleString('en-US', timeOptions);
 		const data = {
 			user: userName,
@@ -52,28 +56,10 @@ const Form = (props) => {
 		reset();
 	};
 
-	useEffect(() => {
-		inputEl.current.focus();
-	});
-
-	const renderField = (field) => (
-		<div className='input-row'>
-			<input {...field.input} ref={inputEl} type='text' className='w-100' />
-			{field.meta.touched && field.meta.error && <span className='error'>{field.meta.error}</span>}
-		</div>
-	);
-
 	return (
 		<form className='px-3' onSubmit={handleSubmit(handleSubmitForm)}>
 			<div className='form-group'>
-				<Field
-					autoFocus
-					name='message'
-					disabled={submitting}
-					required
-					component={renderField}
-					type='text'
-				/>
+				<Field name='message' disabled={submitting} required component={renderField} type='text' />
 			</div>
 		</form>
 	);
@@ -82,5 +68,5 @@ const ConnectedForm = connect(mapPropsToState)(Form);
 
 export default reduxForm({
 	form: 'messageForm',
-	validate,
+	// validate,
 })(ConnectedForm);
