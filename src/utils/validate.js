a4f16287b453a0c20161d37e2d301e60cb19a6d5
EvgeniyPC <antonm33@yandex.ru>
2020-10-01 19:41:24 +0200

    add validate func for channel modals

---
+++ src/utils/validate.js
@@ -0,0 +1,15 @@
+import { SubmissionError } from 'redux-form';
+
+const isLowerCase = (str) => str === str.toLowerCase();
+
+export default (channel, names) => {
+  if (channel && names.includes(channel)) {
+    throw new SubmissionError({ channel: 'This channel name already exist.' });
+  }
+  if (channel && channel.length > 15) {
+    throw new SubmissionError({ channel: 'Can`t be longer than 15 symbols.' });
+  }
+  if (channel && !channel.match(/^[a-z0-9]+$/) || isLowerCase(channel)) {
+    throw new SubmissionError({ channel: 'Channel name must be alphanumeric, in English and lowercase' });
+  }
+}
