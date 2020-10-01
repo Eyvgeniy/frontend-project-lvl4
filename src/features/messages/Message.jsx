61a197c367ae17fa05d4384e02660763540320c0
Evgeniy <antonm33@yandex.ru>
2020-10-01 23:30:49 +0200

    fix lint errors

--- src/features/messages/Message.jsx
+++ src/features/messages/Message.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 const Message = (props) => {
-	const {
-		message: { text, user, time },
-	} = props;
+  const {
+    message: { text, user, time },
+  } = props;
 
-	return (
-		<li className='my-1'>
-			<span className='font-weight-bold'>{`${user} `}</span>
-			<span>{time} </span>
-			<p className='m-0'>{text}</p>
-		</li>
-	);
+  return (
+    <li className="my-1">
+      <span className="font-weight-bold">{`${user} `}</span>
+      <span>{time}</span>
+      <p className="m-0">{text}</p>
+    </li>
+  );
 };
 
 export default Message;
