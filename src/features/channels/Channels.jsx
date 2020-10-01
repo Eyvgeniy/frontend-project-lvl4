4ba422b49e169205f85a56af6470dbad0e5ecccd
EvgeniyPC <antonm33@yandex.ru>
2020-10-01 20:25:41 +0200

    add user name

--- src/features/channels/Channels.jsx
+++ src/features/channels/Channels.jsx
@@ -1,15 +1,10 @@
-import React from 'react';
-import { ListGroup, Button, Nav } from 'react-bootstrap';
+import React, { useContext } from 'react';
+import { ListGroup, Button } from 'react-bootstrap';
 import { connect, useDispatch } from 'react-redux';
 import cn from 'classnames';
-import { addChannel } from './channelsSlice';
+import { showAddChannelModal } from '../modal/modalsSliсe';
+import UserContext from '../../UserContext';
 import { changeChannel } from './channelIdSlice';
-import { addNewChannel } from './channelAddStateSlice';
-import {
-	showAddChannelModal,
-	showRenameChannelModal,
-	showRemoveChannelModal,
-} from '../modal/modalsSliсe';
 
 const mapStateToProps = (state) => {
 	const {
@@ -36,24 +31,22 @@ const Channels = ({ channels, changeChannel, currentChannel }) => {
 		dispatch(showAddChannelModal());
 	};
 
-	const showRenameModal = (channel) => (e) => {
-		e.preventDefault();
-		dispatch(showRenameChannelModal({ channel }));
-	};
-	const showRemoveModal = (channel) => (e) => {
-		e.preventDefault();
-		dispatch(showRemoveChannelModal({ channel }));
-	};
+	const userName = useContext(UserContext);
 
 	return (
 		<>
+			<div className='mb-2 d-flex'>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" height="24px" className='pl-2'>
+					<path d="text-indent:0;text-align:start;line-height:normal;text-transform:none;block-progression:tb;-inkscape-font-specification:Bitstream Vera Sans" d="M 16 5 C 12.145852 5 9 8.1458513 9 12 C 9 14.408843 10.23116 16.55212 12.09375 17.8125 C 8.5266131 19.342333 6 22.881262 6 27 L 8 27 C 8 22.569334 11.569334 19 16 19 C 20.430666 19 24 22.569334 24 27 L 26 27 C 26 22.881262 23.473387 19.342333 19.90625 17.8125 C 21.76884 16.55212 23 14.408843 23 12 C 23 8.1458513 19.854148 5 16 5 z M 16 7 C 18.773268 7 21 9.2267317 21 12 C 21 14.773268 18.773268 17 16 17 C 13.226732 17 11 14.773268 11 12 C 11 9.2267317 13.226732 7 16 7 z" fill="#fff" overflow="visible" fontFamily="Bitstream Vera Sans" />
+				</svg>
+				<span>
+					{userName}
+				</span>
+			</div>
 			<div className='d-flex px-3'>
 				<span>
 					<b>Channels</b>
 				</span>
-				{/* <button type="button" className="btn btn-link mr-1 ml-auto p-0" onClick={showAddModal}>
-          +
-        </button> */}
 				<Button variant='link' className='ml-auto p-0 text-light' onClick={showAddModal}>
 					+
 				</Button>
@@ -73,20 +66,6 @@ const Channels = ({ channels, changeChannel, currentChannel }) => {
 					);
 				})}
 			</ListGroup>
-			{/* {currentChannel.removable ? (
-        <button
-          type="button"
-          className="btn btn-secondary mr-1"
-          onClick={showRenameModal(currentChannel)}
-        >
-          Rename
-        </button>
-      ) : null}
-      {currentChannel.removable ? (
-        <button type="button" className="btn btn-success" onClick={showRemoveModal(currentChannel)}>
-          Remove
-        </button>
-      ) : null} */}
 		</>
 	);
 };
