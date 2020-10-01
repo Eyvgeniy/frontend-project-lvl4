adf8e9cd28af7e07831c4d3aeefa76179f445a5c
Evgeniy <antonm33@yandex.ru>
2020-10-01 23:40:03 +0200

    fix lint problems

--- src/features/messages/MessagesList.jsx
+++ src/features/messages/MessagesList.jsx
@@ -1,22 +1,22 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import Message from './Message';
 
 const mapStateToProps = (state) => {
-	const messages = state.messages.allMessages;
-	const { currentChannelId } = state;
-	const currentMessages = messages.filter(({ channelId }) => channelId === currentChannelId);
-	return { messages: currentMessages };
+  const messages = state.messages.allMessages;
+  const { currentChannelId } = state;
+  const currentMessages = messages.filter(({ channelId }) => channelId === currentChannelId);
+  return { messages: currentMessages };
 };
 
 const MessagesList = (props) => {
-	const { messages } = props;
+  const { messages } = props;
 
-	return (
-		<ul className='list-unstyled overflow-auto flex-grow-1 mt-auto d-flex flex-column-reverse px-3 mb-0'>
-			{messages.map((message) => <Message key={message.id} message={message} />).reverse()}
-		</ul>
-	);
+  return (
+    <ul className="list-unstyled overflow-auto flex-grow-1 mt-auto d-flex flex-column-reverse px-3 mb-0">
+      {messages.map((message) => <Message key={message.id} message={message} />).reverse()}
+    </ul>
+  );
 };
 
 export default connect(mapStateToProps)(MessagesList);
