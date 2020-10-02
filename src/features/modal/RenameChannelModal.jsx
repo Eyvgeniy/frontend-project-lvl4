3bc6f8887513c2a21d3e835ad329581c92fce2cc
EvgeniyPC <antonm33@yandex.ru>
2020-10-02 09:56:21 +0200

    fix lint errors

--- src/features/modal/RenameChannelModal.jsx
+++ src/features/modal/RenameChannelModal.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-shadow */
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
@@ -9,67 +11,75 @@ import { renameChannel } from '../channels/channelRenameStateSlice';
 import validate from '../../utils/validate';
 
 const mapStateToProps = (state) => {
-	const {
-		modal: {
-			props: { id, name },
-		},
-		channels: { byId }
-	} = state;
-	const names = Object.keys(byId).map(key => byId[key].name);
-	return { id, name, names };
+  const {
+    modal: {
+      props: { id, name },
+    },
+    channels: { byId },
+  } = state;
+  const names = Object.keys(byId).map((key) => byId[key].name);
+  return { id, name, names };
 };
 
 const renderField = (field) => (
-	<div className='input-row'>
-		<input {...field.input} type='text' className='w-100' />
-		{field.meta.touched && field.meta.error && (
-			<span className='error text-danger'>{field.meta.error}</span>
-		)}
-	</div>
+  <div className="input-row">
+    <input {...field.input} type="text" className="w-100" />
+    {field.meta.touched && field.meta.error && (
+      <span className="error text-danger">{field.meta.error}</span>
+    )}
+  </div>
 );
 
 const mapDispatch = { closeModal };
 
 const ModalWindow = (props) => {
-	const { handleSubmit, submitting, names, id, name, closeModal } = props;
-	const dispatch = useDispatch();
-	const handleSubmitForm = async ({ channel }) => {
-		validate(channel, names)
-		await dispatch(renameChannel(channel, id));
-		await closeModal();
-	};
+  const {
+    handleSubmit, submitting, names, id, name, closeModal,
+  } = props;
+  const dispatch = useDispatch();
+  const handleSubmitForm = async ({ channel }) => {
+    validate(channel, names);
+    await dispatch(renameChannel(channel, id));
+    await closeModal();
+  };
 
-	const hanleCloseModal = () => {
-		closeModal();
-	};
+  const hanleCloseModal = () => {
+    closeModal();
+  };
 
-	return (
-		<>
-			<Modal show onHide={hanleCloseModal}>
-				<Modal.Header closeButton>
-					<Modal.Title>Rename #{name}</Modal.Title>
-				</Modal.Header>
-				<Form onSubmit={handleSubmit(handleSubmitForm)}>
-					<Form.Group>
-						<Modal.Body>
-							<Field name='channel' disabled={submitting} required component={renderField} type='text' />
-						</Modal.Body>
-					</Form.Group>
-					<Modal.Footer>
-						<Button variant='secondary' type='submit'>
-							Rename
-						</Button>
-						<Button variant='primary' onClick={hanleCloseModal}>
-							Close
-						</Button>
-					</Modal.Footer>
-				</Form>
-			</Modal>
-		</>
-	);
+  return (
+    <>
+      <Modal show onHide={hanleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>{`Rename #${name}`}</Modal.Title>
+        </Modal.Header>
+        <Form onSubmit={handleSubmit(handleSubmitForm)}>
+          <Form.Group>
+            <Modal.Body>
+              <Field
+                name="channel"
+                disabled={submitting}
+                required
+                component={renderField}
+                type="text"
+              />
+            </Modal.Body>
+          </Form.Group>
+          <Modal.Footer>
+            <Button variant="secondary" type="submit">
+              Rename
+            </Button>
+            <Button variant="primary" onClick={hanleCloseModal}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+    </>
+  );
 };
 
 const AddModal = connect(mapStateToProps, mapDispatch)(ModalWindow);
 export default reduxForm({
-	form: 'renameChanneForm',
+  form: 'renameChanneForm',
 })(AddModal);
