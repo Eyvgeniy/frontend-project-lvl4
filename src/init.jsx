785389deabb7b6a2a5b4ebf38976dca5524e3ed2
EvgeniyPC <antonm33@yandex.ru>
2020-10-06 22:35:20 +0200

    fix imports

--- src/init.jsx
+++ src/init.jsx
@@ -1,5 +1,6 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import io from 'socket.io-client';
 import { configureStore } from '@reduxjs/toolkit';
 import React from 'react';
 import ReactDom from 'react-dom';
@@ -8,8 +9,10 @@ import cookie from 'js-cookie';
 import App from './components/App';
 import UserContext from './UserContext';
 import createUserName from './utils/createUserName';
-import initState from '../utils/initState';
-import rootReducer from './reducer';
+import initState from './utils/initState';
+import rootReducer from './reducers';
+import { addChannel, renameChannel, deleteChannel } from './reducers/channel/channelsSlice';
+import { addMessage } from './reducers/messages/messagesSlice';
 
 const init = (gon) => {
   const userName = cookie.get('userName') || createUserName();
@@ -19,6 +22,24 @@ const init = (gon) => {
   const store = configureStore({
     reducer: rootReducer,
     preloadedState,
+  });
+
+  const socket = io();
+
+  socket.on('newMessage', ({ data: { attributes } }) => {
+    store.dispatch(addMessage({ attributes }));
+  });
+
+  socket.on('newChannel', ({ data: { attributes } }) => {
+    store.dispatch(addChannel({ attributes }));
+  });
+
+  socket.on('renameChannel', ({ data: { attributes } }) => {
+    store.dispatch(renameChannel({ attributes }));
+  });
+
+  socket.on('removeChannel', ({ data }) => {
+    store.dispatch(deleteChannel({ data }));
   });
 
   ReactDom.render(
