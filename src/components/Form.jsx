7db5094515ef4bcc054c47b14c98316d9b0d9fa1
Evgeniy <antonm33@yandex.ru>
2020-10-14 21:57:57 +0200

    change redux-form on formik

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useContext } from 'react';
-import { useDispatch, connect } from 'react-redux';
-import { Field, reduxForm, SubmissionError } from 'redux-form';
+import { useSelector } from 'react-redux';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
 import i18next from 'i18next';
-import { has } from 'lodash';
 import { fetchMessage } from '../slices/messages';
 import UserContext from '../UserContext';
 
-const mapPropsToState = (state) => {
-  const {
-    channels: { actualId },
-  } = state;
-  return { actualId };
-};
 const timeOptions = {
   year: 'numeric',
   month: 'short',
@@ -21,51 +15,53 @@ const timeOptions = {
   minute: 'numeric',
 };
 
-const renderField = (field) => (
-  <div className="input-row">
-    <input {...field.input} type="text" className="w-100" />
-    {field.meta.touched && field.meta.error && (
-      <span className="error text-danger">{field.meta.error}</span>
-    )}
-  </div>
-);
-
-const Form = (props) => {
-  const {
-    handleSubmit, submitting, reset, actualId,
-  } = props;
-  const dispatch = useDispatch();
+const MessageForm = () => {
   const userName = useContext(UserContext);
+  const channelId = useSelector((state) => {
+    const {
+      channels: { actualId },
+    } = state;
+    return actualId;
+  });
 
-  const handleSubmitForm = async (values) => {
-    if (!has(values, 'message')) {
-      throw new SubmissionError({ message: i18next.t('errors.message.emptyMessage') });
-    }
-
-    const time = new Date().toLocaleString('en-US', timeOptions);
-    const data = {
-      user: userName,
-      text: values.message,
-      time,
-    };
-    try {
-      await dispatch(fetchMessage(data, actualId));
-    } catch (e) {
-      throw new SubmissionError({ error: e.message });
-    }
-    reset();
-  };
+  const validationSchema = Yup.object({
+    message: Yup.string()
+      .required(i18next.t('errors.message.emptyMessage')),
+  });
 
   return (
-    <form className="px-3" onSubmit={handleSubmit(handleSubmitForm)}>
-      <div className="form-group">
-        <Field name="message" disabled={submitting} required component={renderField} type="text" />
-      </div>
-    </form>
+    <Formik
+      initialValues={{ message: '' }}
+      onSubmit={async ({ message }, actions) => {
+        const time = new Date().toLocaleString('en-US', timeOptions);
+        const data = {
+          user: userName,
+          text: message,
+          time,
+        };
+        actions.setSubmitting(false);
+        await fetchMessage(data, channelId);
+        actions.resetForm();
+      }}
+      validationSchema={validationSchema}
+      validateOnBlur={false}
+      validateOnChange={false}
+    >
+      {(props) => (
+        <form id="simpleForm" onSubmit={props.handleSubmit} className="px-3 pb-1">
+          <input
+            name="message"
+            type="text"
+            onChange={props.handleChange}
+            onBlur={props.handleBlur}
+            value={props.values.message}
+            className="w-100"
+          />
+          {props.errors.message && props.touched.message ? <div className="text-danger">{props.errors.message}</div> : null}
+        </form>
+      )}
+    </Formik>
   );
 };
-const ConnectedForm = connect(mapPropsToState)(Form);
 
-export default reduxForm({
-  form: 'messageForm',
-})(ConnectedForm);
+export default MessageForm;
