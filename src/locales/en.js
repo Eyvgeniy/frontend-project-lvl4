a78c8577e0b6080bc198c17497b4094a73cb616a
EvgeniyPC <antonm33@yandex.ru>
2020-10-28 15:44:13 +0200

    Add error for internet connection

--- src/locales/en.js
+++ src/locales/en.js
@@ -7,9 +7,11 @@ export default {
         existName: 'This channel name already exist.',
         longName: 'Can`t be longer than 15 symbols.',
         alphanumeric: 'Channel name must be alphanumeric, in English and lowercase',
+        internet: 'Something went wrong, pleasy try again',
       },
       message: {
         emptyMessage: 'Can`t send empty message',
+        internet: 'Something went wrong, pleasy try again',
       },
     },
     modals: {
